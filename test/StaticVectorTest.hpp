ef48a15afa143b680fe150251f282eb31bec264a
Murat Bolu <murat.bolu@tbtk.gov.tr>
2024-07-02 11:48:20 +0300

    Write more unit tests

--- test/StaticVectorTest.hpp
+++ test/StaticVectorTest.hpp
@@ -12,6 +12,11 @@ protected:
 		vec1[0] = 3;
 		vec1[1] = 5;
 		vec1[2] = 7;
+
+		vec2[0][0] = 12;
+		vec2[4][3] = 15;
 	}
+	StaticVector<char, 2> vec0; // uninitialized
 	StaticVector<int, 3> vec1;
+	StaticVector<StaticVector<unsigned, 4>, 5> vec2;
 };
