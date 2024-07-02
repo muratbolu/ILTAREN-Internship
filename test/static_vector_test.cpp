56a6a0771531c7b8fd865f90e0427b57a43de8b8
Murat Bolu <murat.bolu@tbtk.gov.tr>
2024-07-02 10:21:23 +0300

    Improve tests

--- test/static_vector_test.cpp
+++ test/static_vector_test.cpp
@@ -2,10 +2,25 @@
 
 #include "static_vector.tpp"
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-	// Expect two strings not to be equal.
-	EXPECT_STRNE("hello", "world");
-	// Expect equality.
-	EXPECT_EQ(7 * 6, 42);
+class StaticVectorTest : public testing::Test
+{
+protected:
+	StaticVectorTest()
+	{
+		vec1[0] = 3;
+		vec1[1] = 5;
+		vec1[2] = 7;
+	}
+	static_vector<int, 3> vec1;
+};
+
+TEST_F(StaticVectorTest, Initialization) {
+	EXPECT_EQ(vec1.size(), 3);
+	EXPECT_EQ(vec1.empty(), false);
+}
+
+TEST_F(StaticVectorTest, Assignment) {
+	EXPECT_EQ(vec1[0], 3);
+	EXPECT_EQ(vec1[1], 5);
+	EXPECT_EQ(vec1[2], 7);
 }
