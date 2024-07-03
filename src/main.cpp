c2dcaf31ebfe0f0aa1d11f280e899d01b40c71ed
Murat Bolu <murat.bolu@tbtk.gov.tr>
2024-07-03 17:06:07 +0300

    Change main and test structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
-#include <gtest/gtest.h>
+#include <iostream>
 
-#include "ContIterTest.hpp"
-#include "LinkedListTest.hpp"
-#include "NodeIterTest.hpp"
-#include "NodeTest.hpp"
-#include "ObjectPoolTest.hpp"
-#include "StaticVectorTest.hpp"
-
-int main(int argc, char** argv) {
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+int main()
+{
+	std::cout << "Hello, World!\n";
+	return 0;
 }
---
+++ src/tests.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+#include "ContIterTest.hpp"
+#include "LinkedListTest.hpp"
+#include "NodeIterTest.hpp"
+#include "NodeTest.hpp"
+#include "ObjectPoolTest.hpp"
+#include "StaticVectorTest.hpp"
+
+int main(int argc, char** argv) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
