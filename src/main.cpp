e8d83ea07c91db9090737aef151bb54124fb9b5b
Murat Bolu <murat.bolu@tbtk.gov.tr>
2024-07-03 17:22:52 +0300

    Remove std:: prefixes

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,24 @@ int main(int argc, char** argv)
 {
 	if (argc != 2)
 	{
-		int rc = std::fputs(
+		int rc = fputs(
 			"Provide exactly one argument, which should be the input file.\n", stderr);
 		if (rc == EOF)
 		{
-			std::perror("Invalid argument number");
+			perror("Invalid argument number");
 		}
 		return 1; // EXIT_FAILURE
 	}
 
-	FILE* fp = std::fopen(argv[1], "r");
+	FILE* fp = fopen(argv[1], "r");
 	if (fp == nullptr)
 	{
-		std::perror("Could not open file");
+		perror("Could not open file");
 		return 1;
 	}
 
 	// TODO: get chars
 
-	std::fclose(fp);
+	fclose(fp);
 	return 0;
 }
